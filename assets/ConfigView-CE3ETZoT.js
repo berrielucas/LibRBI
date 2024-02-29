676a78b18cd4e26cf2c49d9eec53f747cfbdd5ce
Lucas Berriel <134228675+berrielucas@users.noreply.github.com>
2024-02-29 13:01:57 -0300

    Delete assets directory

--- assets/ConfigView-CE3ETZoT.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as n,o as c}from"./index-kAl3etpa.js";const o={};function r(a,t){return c(),n("main",null," Tela de Configurações ")}const _=e(o,[["render",r]]);export{_ as default};
